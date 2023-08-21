d0aa556cec650852507a7d9a7f4f75c1938635b9
shrutanten <shrutanten.work@gmail.com>
2023-08-21 16:16:11 +0530

    Finish UserRepo

--- src/main/java/com/example/repository/UserRepositoryImpl.java
+++ src/main/java/com/example/repository/UserRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.example.repository;
 
 import com.example.entity.*;
 import java.util.*;
+import java.util.stream.*;
 
 // the interface for user repository
 interface IUserRepository {
@@ -16,6 +17,54 @@ interface IUserRepository {
   List<User> getAllUsers();
 }
 
+// class implementation for the interface
 public class UserRepositoryImpl implements IUserRepository {
+  private List<User> users = new ArrayList<>();
 
+  public User addUser(User user) {
+    // don't add if already present
+    if (users.contains(user))
+      return user;
+
+    users.add(user);
+    return user;
+  }
+
+  public void deleteUser(int userId) {
+    users.removeIf((userObj) -> userObj.getId() == userId);
+  }
+
+  public User updateUser(int userId, String newEmail) {
+    this.users
+        .stream()
+        .filter((userObj) -> userObj.getId() == userId)
+        .forEach((userObj) -> userObj.setEmail(newEmail));
+
+    return this.users
+        .stream()
+        .filter((userObj) -> userObj.getId() == userId)
+        .collect(Collectors.toList())
+        .get(0);
+  }
+
+  public User updateUser(User newUser) {
+    this.users
+        .stream()
+        .filter((userObj) -> userObj.getId() == newUser.getId())
+        .forEach((userObj) -> {
+          userObj.setEmail(newUser.getEmail());
+          userObj.setName(newUser.getName());
+          // id cannot be changed
+        });
+
+    return this.users
+        .stream()
+        .filter((userObj) -> userObj.getId() == newUser.getId())
+        .collect(Collectors.toList())
+        .get(0);
+  }
+
+  public List<User> getAllUsers() {
+    return users;
+  }
 }
